d56aa32615d39614297b9061510f881ffb0c34c2
Astha <astharana0123@gmail.com>
2023-07-25 17:43:40 +0530

    Added create city API

---
+++ src/repositories/city-repositoty.js
@@ -0,0 +1,9 @@
+const CrudRepository = require('./crud-repository');
+const { city } = require('../models');
+
+class CityRepository extends CrudRepository {
+    constructor(){
+        super(city);
+    }
+}
+module.exports = CityRepository;
