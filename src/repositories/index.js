6e4f51bd01cd6e373d51f7899b035df808630a27
Astha <astharana0123@gmail.com>
2023-07-31 23:30:49 +0530

    Added changes

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     AirplaneRepository: require('./airplane-repository'),
-    CityRepository: require ('./city-repositoty')
+    CityRepository: require ('./city-repositoty'),
+    Airportrepository: require('./airport-repository')
 }
